eadd5cb86d3e00631ddfcb2be15570a1861eb630
島崎塁 <mitsuba19930328@gmail.com>
2020-10-11 14:47:09 +0900

    controller & routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "top#index"
+  resources :users, only: %i(index show destroy edit)
+  resources :questions, only: %i(index create show)
+  resources :answers, only: %i(create)
 end
