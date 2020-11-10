1a2c27d62b73a331e5041e5035922203ceb7f128
島崎塁 <mitsuba19930328@gmail.com>
2020-11-11 00:59:04 +0900

    create-about-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "top#index"
+  get  "top/about"  => "top#about"
   resources :users, only: %i(show destroy edit)
   resources :questions, only: %i(index create show new destroy)
   resources :answers, only: %i(create)
